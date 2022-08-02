3a0f3d9dee79a60d797448b8335c4ed048bab637
Erkan Oner <techno.erkan@gmail.com>
2022-08-02 21:25:17 +0300

    ilk commit

---
+++ JavaBootCampVideo1/src/test/Info.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Info {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("ERKAN ONER");
+		
+		
+		
+	}
+
+}
---
+++ JavaBootCampVideo2/src/oop/Product.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class Product {
+	
+	int id;
+	String name ;
+	double unitPrice;
+	String detail ;
+	
+	
+}
